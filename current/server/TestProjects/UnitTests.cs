d4853499ad74073ff7e9f49f2afadf4612d84e2d
alan <alan@a4613560-6807-4f28-a831-3b17b52ddf2c>
2010-01-05 13:16:21 +0000

    add nunit tests

--- current/server/TestProjects/UnitTests.cs
+++ current/server/TestProjects/UnitTests.cs
@@ -1,8 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+#if !nunit
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestContext = System.String;
+using DeploymentItem = NUnit.Framework.DescriptionAttribute;
+#endif
+
 using netGui.RuleEngine;
 using netGui.RuleEngine.ruleItems;
+using System.Collections.Generic;
+using System;
 
 namespace TestProjects
 {
