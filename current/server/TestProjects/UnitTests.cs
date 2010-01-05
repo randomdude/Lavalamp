0699d20707aad352b8705105e4c65e40786177b6
alan <alan@a4613560-6807-4f28-a831-3b17b52ddf2c>
2010-01-05 13:38:29 +0000

    restore tests

--- current/server/TestProjects/UnitTests.cs
+++ current/server/TestProjects/UnitTests.cs
@@ -15,89 +15,89 @@ namespace TestProjects
         private const string filename = @"C:\lavalamp\current\server\netGui\RuleEngine\ruleItems\pythonScripts\test.py";
         private const string filenameCategory = @"C:\lavalamp\current\server\netGui\RuleEngine\ruleItems\pythonScripts\test-category.py";
 
-        ///// <summary>
-        ///// Verify load of an example python ruleItem, ensuring that the getPinInfo operates.
-        ///// </summary>
-        //[TestMethod]
-        //public void testLoadOfPythonRuleItem()
-        //{
-        //    pythonEngine myEng = new pythonEngine(filename);
+        /// <summary>
+        /// Verify load of an example python ruleItem, ensuring that the getPinInfo operates.
+        /// </summary>
+        [TestMethod]
+        public void testLoadOfPythonRuleItem()
+        {
+            pythonEngine myEng = new pythonEngine(filename);
 
-        //    ruleItem_python testItem = new ruleItem_python(myEng);
+            ruleItem_python testItem = new ruleItem_python(myEng);
 
-        //    Assert.AreEqual("test python rule item", testItem.ruleName());
+            Assert.AreEqual("test python rule item", testItem.ruleName());
 
-        //    Dictionary<String, pin> pins = testItem.getPinInfo();
+            Dictionary<String, pin> pins = testItem.getPinInfo();
 
-        //    Assert.IsTrue(testItem.category == "");
+            Assert.IsTrue(testItem.category == "");
 
-        //    Assert.IsTrue(pins.Count == 2 );
-        //    Assert.IsTrue(pins.ContainsKey("myInputPin"));
-        //    Assert.IsTrue(pins.ContainsKey("myOutputPin"));
+            Assert.IsTrue(pins.Count == 2 );
+            Assert.IsTrue(pins.ContainsKey("myInputPin"));
+            Assert.IsTrue(pins.ContainsKey("myOutputPin"));
 
-        //    Assert.IsTrue(pins["myOutputPin"].direction == pinDirection.output);
-        //    Assert.IsTrue(pins["myInputPin"].direction == pinDirection.input);
-        //}
+            Assert.IsTrue(pins["myOutputPin"].direction == pinDirection.output);
+            Assert.IsTrue(pins["myInputPin"].direction == pinDirection.input);
+        }
 
-        //[TestMethod]
-        //public void testLoadOfPythonRuleItemWithCategory()
-        //{
-        //    pythonEngine myEng = new pythonEngine(filenameCategory);
+        [TestMethod]
+        public void testLoadOfPythonRuleItemWithCategory()
+        {
+            pythonEngine myEng = new pythonEngine(filenameCategory);
 
-        //    ruleItem_python testItem = new ruleItem_python(myEng);
+            ruleItem_python testItem = new ruleItem_python(myEng);
 
-        //    Assert.AreEqual("test category", myEng.category);
-        //    Assert.AreEqual("test category", testItem.category);
-        //}
+            Assert.AreEqual("test category", myEng.category);
+            Assert.AreEqual("test category", testItem.category);
+        }
 
-        //[TestMethod]
-        //public void testLoadOfPythonRuleItemWithParameters()
-        //{
-        //    pythonEngine myEng = new pythonEngine(filenameParameters);
+        [TestMethod]
+        public void testLoadOfPythonRuleItemWithParameters()
+        {
+            pythonEngine myEng = new pythonEngine(filenameParameters);
 
-        //    ruleItem_python testItem = new ruleItem_python(myEng);
+            ruleItem_python testItem = new ruleItem_python(myEng);
 
-        //    Assert.AreEqual(2, myEng.Parameters.Count);
-        //    Assert.AreEqual("first value", myEng.Parameters["clampToZero"]);
-        //    Assert.AreEqual("second value", myEng.Parameters["IAmTheSecondOption"]);
+            Assert.AreEqual(2, myEng.Parameters.Count);
+            Assert.AreEqual("first value", myEng.Parameters["clampToZero"]);
+            Assert.AreEqual("second value", myEng.Parameters["IAmTheSecondOption"]);
 
-        //    Assert.AreEqual(2, testItem.parameters.Count );
-        //    Assert.AreEqual("first value", testItem.parameters["clampToZero"]);
-        //    Assert.AreEqual("second value", testItem.parameters["IAmTheSecondOption"]);
-        //}
+            Assert.AreEqual(2, testItem.parameters.Count );
+            Assert.AreEqual("first value", testItem.parameters["clampToZero"]);
+            Assert.AreEqual("second value", testItem.parameters["IAmTheSecondOption"]);
+        }
 
-        //[TestMethod]
-        //public void testFunctionalityOfPythonRuleItemWithParameters()
-        //{
-        //    pythonEngine myEng = new pythonEngine(filenameParameters);
+        [TestMethod]
+        public void testFunctionalityOfPythonRuleItemWithParameters()
+        {
+            pythonEngine myEng = new pythonEngine(filenameParameters);
 
-        //    ruleItem_python testItem = new ruleItem_python(myEng);
+            ruleItem_python testItem = new ruleItem_python(myEng);
 
-        //    testItem.parameters["clampToZero"] = "NO U";
+            testItem.parameters["clampToZero"] = "NO U";
 
-        //    testItem.pinStates["myInputPin"] = true;
-        //    testItem.evaluate();
-        //    Assert.IsTrue( (bool)testItem.pinStates["myOutputPin"] );
+            testItem.pinStates["myInputPin"] = true;
+            testItem.evaluate();
+            Assert.IsTrue( (bool)testItem.pinStates["myOutputPin"] );
 
-        //    testItem.parameters["clampToZero"] = "yes";
-        //    testItem.evaluate();
-        //    Assert.IsFalse((bool)testItem.pinStates["myOutputPin"]);
-        //}
+            testItem.parameters["clampToZero"] = "yes";
+            testItem.evaluate();
+            Assert.IsFalse((bool)testItem.pinStates["myOutputPin"]);
+        }
 
-        //[TestMethod]
-        //public void testFunctionalityOfPythonRuleItem()
-        //{
-        //    pythonEngine myEng = new pythonEngine(filename);
+        [TestMethod]
+        public void testFunctionalityOfPythonRuleItem()
+        {
+            pythonEngine myEng = new pythonEngine(filename);
 
-        //    ruleItem_python testItem = new ruleItem_python(myEng);
-        //    testItem.pinStates["myInputPin"] = false;
-        //    testItem.evaluate();
-        //    Assert.IsTrue(((bool)testItem.pinStates["myOutputPin"]) == false);
+            ruleItem_python testItem = new ruleItem_python(myEng);
+            testItem.pinStates["myInputPin"] = false;
+            testItem.evaluate();
+            Assert.IsTrue(((bool)testItem.pinStates["myOutputPin"]) == false);
 
-        //    testItem.pinStates["myInputPin"] = true;
-        //    testItem.evaluate();
-        //    Assert.IsTrue(((bool)testItem.pinStates["myOutputPin"]) == true);
-        //}
+            testItem.pinStates["myInputPin"] = true;
+            testItem.evaluate();
+            Assert.IsTrue(((bool)testItem.pinStates["myOutputPin"]) == true);
+        }
 
 
         // This will fail unless the emailItem is bodged to store your login credentials/settings.
