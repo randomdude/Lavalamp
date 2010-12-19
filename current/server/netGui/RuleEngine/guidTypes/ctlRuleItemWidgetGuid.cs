ed8cac83ef081d9a8ded26957e32e1ebd030351f
alan <alan@a4613560-6807-4f28-a831-3b17b52ddf2c>
2010-12-19 12:49:06 +0000

    forgot this file

---
+++ current/server/netGui/RuleEngine/guidTypes/ctlRuleItemWidgetGuid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace netGui.RuleEngine
+{
+    public class ctlRuleItemWidgetGuid
+    {
+        public Guid id = Guid.Empty;
+
+        public ctlRuleItemWidgetGuid() {}
+
+        public ctlRuleItemWidgetGuid(string newGuid)
+        {
+            this.id = new Guid(newGuid);
+        }
+
+        public new string ToString()
+        {
+            return this.id.ToString();
+        }
+    }
+}
