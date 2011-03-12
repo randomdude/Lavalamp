75f35c1965b4747bb44a751a2394b4778c85d54b
aliz-svn <aliz-svn@a4613560-6807-4f28-a831-3b17b52ddf2c>
2011-03-12 21:45:07 +0000

    commit forgotten files

---
+++ current/server/lavalampdll/testing.c
@@ -0,0 +1,31 @@
+#include "protocol.h"
+#include "comms.h"
+#include "commands.h"
+
+// Create a new appConfig_t and fill it wtih some test values. This can be called from the other side of our
+// p/invoke to verify that the p/invoke is operating correctly.
+appConfig_t* getTestConfig()
+{
+	appConfig_t* toRet;
+
+	toRet = (appConfig_t*)malloc(sizeof(appConfig_t));
+
+	toRet->nodeid = 0x01;
+	toRet->sensorid = 0x02;
+	toRet->portname = "some port";
+	toRet->useEncryption = TRUE;
+	toRet->key[0] = 0x01020304;
+	toRet->key[1] = 0x05060708;
+	toRet->key[2] = 0x090a0b0c;
+	toRet->key[3] = 0x0d0e0f00;
+	toRet->verbose = 0x22;
+	toRet->hnd = (HANDLE)0x10111213;
+	toRet->machineoutput = FALSE;
+	toRet->com_timeout = 0x14;
+	toRet->assume_synced = TRUE;
+	toRet->retries = 0x15;
+	toRet->isSerialPort = FALSE;
+	toRet->injectFaultInvalidResponse = TRUE;
+
+	return toRet;
+}
