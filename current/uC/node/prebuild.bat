db0b2fbf20b7deeb026b650f0806f336080dfadd
aliz-svn <aliz-svn@a4613560-6807-4f28-a831-3b17b52ddf2c>
2011-10-13 08:28:29 +0000

    Remove now-unnecessary autogen step

--- current/uC/node/prebuild.bat
+++ current/uC/node/prebuild.bat
@@ -7,5 +7,4 @@ perl regen.pl template_cmd-fade-sensor.asm > cmd-fade-sensor.asm
 perl regen.pl template_cmd-get-sensor-type.asm > cmd-get-sensor-type.asm
 perl regen.pl template_memoryplacement.h > memoryplacement.h
 perl regen.pl template_idle-pwm.asm > idle-pwm.asm
-perl regen.pl template_idle-servo.asm > idle-servo.asm
 REM perl regen.pl template_idle-triac.asm > idle-triac.asm
