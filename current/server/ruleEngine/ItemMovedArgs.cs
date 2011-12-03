8090d182c72d9e76304d2de5896085722425143f
kat-svn <kat-svn@a4613560-6807-4f28-a831-3b17b52ddf2c>
2011-12-03 09:53:34 +0000

    missing file in last commit :s

---
+++ current/server/ruleEngine/ItemMovedArgs.cs
@@ -0,0 +1,11 @@
+using System.Drawing;
+
+namespace ruleEngine
+{
+    public class ItemMovedArgs
+    {
+        public pinDirection pinDirection { get;set;}
+
+        public Point point{get; set;}
+    }
+}
